2b4bb7c1694c22672a16bdee028346c4a933bab3
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2019-12-30 17:03:23 +0800

    '主页轮播组件开发'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <home-header></home-header>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+  </div>
+
 </template>
 <script>
   import homeHeader from './components/header'
-    export default {
-        name: "Home",
-      components: {
-          homeHeader,
-      }
+  import homeSwiper from './components/swiper'
+  export default {
+    name: "Home",
+    components: {
+      homeHeader,
+      homeSwiper,
     }
+  }
 </script>
 <style scoped>
 </style>
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide  v-for="item of imgList" :key="item.id">
+<!--        这里是有个大bug的，学完之后再回来检查-->
+<!--        <img class="swiper-img" src="../../../assets/images/banner2.jpg" alt="">-->
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "homeSwiper",
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true,
+          observer:true,
+        },
+        imgList: [{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/vs_ceph_vs_tts/e3c66e31-3bab-4c5a-a1ff-a7d637716a0f.jpg'
+        },{
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/vs_ceph_vs_tts/f1ad20aa-0ede-4283-b844-b13c0d980415.jpg'
+        }, {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/vs_ceph_vs_tts/73a9307d-892d-4e3f-b7e7-85e4b3aced18.jpg'
+          }],
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet
+    background: red
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom 35%
+    background: red;
+    .swiper-img
+      width: 100%
+
+</style>
